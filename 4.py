c6fabea8529c828d91b6c8e9124a9ddbe9643b4a
Will Keleher <will.keleher@gmail.com>
2022-12-03 22:33:13 -0700

    use map rather than list comprehension

--- 4.py
+++ 4.py
@@ -2,15 +2,15 @@ input = open("4.input").read()
 
 count = 0
 for row in input.split("\n"):
-    [x1, y1, x2, y2] = [int(d) for d in row.replace("-", ",").split(",")]    
+    [x1, y1, x2, y2] = map(int, row.replace("-", ",").split(","))
     if (x1 >= x2 and y1 <= y2) or (x2 >= x1 and y2 <= y1):
         count += 1
 
-print(f"part 1 {count}")
+print(f"part 1: {count}")
 
 count = 0
 for row in input.split("\n"):
-    [x1, y1, x2, y2] = [int(d) for d in row.replace("-", ",").split(",")]    
+    [x1, y1, x2, y2] = map(int, row.replace("-", ",").split(","))
     if max(x1, x2) <= min(y1, y2):
         count += 1
 
