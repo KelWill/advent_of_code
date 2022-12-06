79de2da76b099ae865c24ebd226f851861f68fe9
will <will@classdojo.com>
2022-12-05 22:13:44 -0700

    short version

--- 6.py
+++ 6.py
@@ -1,21 +1,8 @@
 input = open("6.input").read()
 # input = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
 
-counts = {}
 n = 14
-for i in range(0, n):
-  if not input[i] in counts:
-    counts[input[i]] = 0
-  counts[input[i]] += 1
-
-for i in range(n, len(input)):
-  counts[input[i - n]] -= 1
-  if counts[input[i - n]] == 0:
-    del counts[input[i - n]]
-  if not input[i] in counts:
-    counts[input[i]] = 0
-  counts[input[i]] += 1
-  print(len(counts.keys()))
-  if len(counts.keys()) == n:
-    print(i + 1)
+for i in range(len(input)):
+  if len(set(input[i:i + n])) == n:
+    print(i + n)
     break
