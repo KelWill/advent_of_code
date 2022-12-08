556b682dcf048570fb076b8f56c52cd5e186b860
will <will@classdojo.com>
2022-12-08 09:18:29 -0700

    day8 small cleanups

--- 8.py
+++ 8.py
@@ -6,43 +6,40 @@ input = open("8.input").read()
 # 33549
 # 35390"""
 
-grid = []
-
-for row in input.split("\n"):
-  grid.append(list(int(x) for x in list(row)))
+grid = [list(map(int, list(row))) for row in input.split("\n")]
 
 best_view_score = 0
+externally_visible = 0
+
 for r0 in range(len(grid)):
-  trees = grid[r0]
+  row = grid[r0]
 
-  for c0 in range(len(trees)):
-    tree_height = trees[c0]
-    seen = False
+  for c0 in range(len(row)):
+    total_view_score = 1
 
-    r = r0
-    c = c0
-
-    total_view_score = 1  
-
+    visible = False
     for [dr, dc] in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
       r = r0
       c = c0
       view_score = 0
 
-      while r < len(grid) and c < len(trees) and c >= 0 and r >= 0:
+      while True:
         r += dr
         c += dc
 
-        if r >= len(grid) or c >= len(trees) or c < 0 or r < 0:
-          total_view_score *= view_score
+        if not (0 <= r < len(grid) and 0 <= c < len(row)):
+          visible = True
           break
-        elif grid[r][c] >= tree_height:
+        elif grid[r][c] >= grid[r0][c0]:
           view_score += 1
-          total_view_score *= view_score
           break
         else:
           view_score += 1
 
+      total_view_score *= view_score
+    if visible:
+      externally_visible += 1
     best_view_score = max(total_view_score, best_view_score)
 
-print(best_view_score)
+print(f"part1 {externally_visible}")
+print(f"part2 {best_view_score}")
