3bd596f8708885bac1ae4bccab087e3d40dd071a
Will Keleher <will.keleher@gmail.com>
2023-12-09 19:53:14 +0000

    remove regular expression

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,13 +1,6 @@
-import re
-
-
 ex = """0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45"""
-
-
-def get_ints(s):
-    return list(map(int, re.findall(r'-?\d+', s)))
 
 
 def get_next(seq):
@@ -21,8 +14,9 @@ def main(s):
     part1 = 0
     part2 = 0
     for l in s.split("\n"):
-        part1 += get_next(get_ints(l))[-1]
-        part2 += get_next(list(reversed(get_ints(l))))[-1]
+        seq = list(map(int, l.split()))
+        part1 += get_next(seq)[-1]
+        part2 += get_next(list(reversed(seq)))[-1]
 
     return (part1, part2)
 
