d761907549921f6f053cc0f1496428143041e37f
Will Keleher <will.keleher@gmail.com>
2023-12-09 19:56:50 +0000

    2023 day 9 tighten up even mroe

--- 2023/day9.py
+++ 2023/day9.py
@@ -5,20 +5,14 @@ ex = """0 3 6 9 12 15
 
 def get_next(seq):
     if all(n == 0 for n in seq):
-        return [0] + seq
-    lower = get_next([b - a for a, b in zip(seq, seq[1:])])
-    return seq + [seq[-1] + lower[-1]]
+        return 0
+    return seq[-1] + get_next([b - a for a, b in zip(seq, seq[1:])])
 
 
 def main(s):
-    part1 = 0
-    part2 = 0
-    for l in s.split("\n"):
-        seq = list(map(int, l.split()))
-        part1 += get_next(seq)[-1]
-        part2 += get_next(list(reversed(seq)))[-1]
-
-    return (part1, part2)
+    seqs = [list(map(int, l.split())) for l in s.split("\n")]
+    return (sum(get_next(seq) for seq in seqs),
+            sum(get_next(list(reversed(seq))) for seq in seqs))
 
 
 print("ex", main(ex))
