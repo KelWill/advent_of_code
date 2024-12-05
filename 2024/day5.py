4b685fe6095dbbc2361798fa5afa6342898a0ce1
Will Keleher <will@classdojo.com>
2024-12-05 16:07:43 +0000

    tiny cleanup

--- 2024/day5.py
+++ 2024/day5.py
@@ -6,26 +6,22 @@ for rule in rule_string.split("\n"):
     before, after = rule.split("|")
     rules[before].add(after)
 
-
 def find_valid_order (page):
     for c in page.keys():
         for after in rules[c]:
             if after in page and page[after] < page[c]:
-                i = page[after]
-                page[after] = page[c]
-                page[c] = i
+                page[after], page[c] = page[c], page[after]
                 return find_valid_order(page)
     return page
 
 p1 = 0
 p2 = 0
 for line in pages_string.split("\n"):
-    valid = True
     page = { c : i for i, c in enumerate(line.split(","))}
+    valid = True
     for c in page.keys():
-        for after in rules[c]:
-            if after in page and page[after] < page[c]:
-                valid = False
+        if any(after in page and page[after] < page[c]):
+            valid = False
     if valid:
         p1 += int(line.split(",")[len(page) // 2])
     if not valid:
