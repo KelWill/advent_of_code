9087d7c7a621afb6766bbf04c678f398fc9467c7
Will Keleher <will@classdojo.com>
2024-12-09 15:38:38 +0000

    day8

---
+++ 2024/day8.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+import math
+real = open("./day8.input").read()
+
+ex = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"""
+
+def main (s):
+    lines = s.split("\n")
+    G = { r + c * 1j: lines[r][c] for r in range(len(lines)) for c in range(len(lines[r])) }
+    
+    points_by_type = defaultdict(list)
+    for pos, c in G.items():
+        if c == ".":
+            continue
+        points_by_type[c].append(pos)
+    
+    result = set()
+    for positions in points_by_type.values():
+        pairs = [(a, b) for a in positions for b in positions if a != b]
+        for a, b in pairs:
+            vec = b - a
+            for v in (vec, -vec):
+                for curr in (a, b):
+                    while curr in G:
+                        result.add(curr)
+                        curr += v
+
+    return len(result)
+
+print(main(ex))
+print(main(real))
+
