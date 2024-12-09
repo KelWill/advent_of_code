1752d7fc723040728c24031e6a50e02d21a413e8
Will Keleher <will@classdojo.com>
2024-12-09 15:38:34 +0000

    reversed array

--- 2024/day7.py
+++ 2024/day7.py
@@ -12,26 +12,25 @@ def get_operators (n):
         for rest in get_operators(n - 1):
             yield [op] + rest
 
-
-p1 = 0
+ans = 0
 for line in input.split("\n"):
     goal, *rest = [*map(int, re.findall(r"\d+", line))]
     op_count = len(rest) - 1
     for ops in get_operators(op_count):
-        nums = [] + rest
-        n = nums.pop(0)
+        nums = [*reversed([] + rest)]
+        n = nums.pop()
         while nums:
             op = ops.pop()
             if op == "||":
-                n = int(str(n) + str(nums.pop(0)))
+                n = int(str(n) + str(nums.pop()))
             elif op == "*":
-                n *= nums.pop(0)
+                n *= nums.pop()
             else:
-                n += nums.pop(0)
+                n += nums.pop()
             if n > goal:
                 break
         if n == goal:
-            p1 += goal
+            ans += goal
             break
 
-print(p1)
+print(ans)
