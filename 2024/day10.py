89b1d10c09e93416c88fc8e6c45708afecea845d
Will Keleher <will@classdojo.com>
2024-12-10 19:59:43 +0000

    day10 slightly golfed

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,31 +1,17 @@
 from collections import defaultdict
 real = open("./day10.input").read()
-
-ex = """89010123
-78121874
-87430965
-96549874
-45678903
-32019012
-01329801
-10456732"""
-
+ex = open("./day10.example").read()
 
 def main (s):
     G = defaultdict(lambda: -1) | {r + c * 1j: int(char) for r, row in enumerate(s.split("\n")) for c, char in enumerate(row) }
-    p1 = set()
-    p2 = 0
+    result = []
     todo = [(p, p) for p, e in G.items() if e == 0]
     while todo:
         p, start = todo.pop()
-        e = G[p]
-        if e == 9:
-            p2 += 1
-            p1.add((start, p))
-        for d in (-1, 1, 1j, -1j):
-            if G[p + d] == e + 1:
-                todo.append((p + d, start))
+        if G[p] == 9:
+            result.append((start, p))
+        todo += [(p + d, start) for d in (-1, 1, 1j, -1j) if G[p + d] == G[p] + 1]
 
-    return len(p1), p2
+    return len(set(result)), len(result)
 
 print(main(ex), main(real))
