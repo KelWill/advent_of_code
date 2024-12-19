9bd1031354d8d5bf98f1478fbbacb164c152e634
Will Keleher <will@classdojo.com>
2024-12-19 16:39:25 +0000

    tighten things up a bit

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,43 +1,17 @@
 import functools
-
-
 real = open("./day19.input").read()
-ex = """r, wr, b, g, bwu, rb, gb, br
-
-brwrr
-bggr
-gbbr
-rrbgbr
-ubwu
-bwurrg
-brgr
-bbrgwb"""
-
 
 @functools.cache
 def ways_possible (pattern, remaining):
-    ways = 0
     if pattern == "":
-        return 1
-    for i, r in enumerate(remaining):
-        if not pattern.startswith(r):
-            continue
-        ways += ways_possible(pattern[len(r):], remaining)
-    return ways
-
-
+        return 1    
+    return sum(ways_possible(pattern[len(r):], remaining) for r in remaining if pattern.startswith(r))
 
 def main (s):
     towels, patterns = s.split("\n\n")
     towels = tuple(towels.split(", "))
     patterns = patterns.split("\n")
+    result = [*(ways_possible(p, towels) for p in patterns)]
+    return sum(r > 0 for r in result), sum(result)
 
-    return sum(ways_possible(p, towels) for p in patterns)
-
-print(main(ex))
 print(main(real))
-
-
-
-
-
